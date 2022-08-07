b62f270ef3fe0efea776b062da02c416db440b5b
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2022-08-07 23:34:17 +0300

    Reorganise maps, add a static website generator and a GitHub Action for it

---
+++ template/generate.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import chevron
+import os
+import json
+
+info = {'categories': []}
+
+with open('template/map.html.mustache', 'r') as mapTemplate:
+	for category in os.listdir('maps'):
+		print('Found a category:', category)
+
+		categoryDict = None
+		with open(f'maps/{category}/{category}.json', 'r') as f:
+			categoryDict = json.load(f)
+		categoryDict['id'] = category
+		categoryDict['maps'] = []
+
+		for map in [f.name for f in os.scandir('maps/' + category) if f.is_dir()]:
+			print('Found a map:', map)
+
+			mapDict = dict()
+			path = f'maps/{category}/{map}/{map}.'
+			j = None
+			with open(path + 'json') as f:
+				j = json.load(f)
+
+			mapDict['name'] = map
+			mapDict['author'] = j['author']
+			mapDict['description'] = j['description']
+			mapDict['path'] = path
+			mapDict['tags'] = [{'tag': tag} for tag in j['tags']]
+
+			with open(path + 'html', 'w') as f:
+				f.write(chevron.render(mapTemplate, mapDict))
+
+			categoryDict['maps'].append(mapDict)
+
+		info['categories'].append(categoryDict)
+
+print(info)
+
+with open('template/index.html.mustache', 'r') as indexTemplate, open('index.html', 'w') as f:
+	f.write(chevron.render(indexTemplate, info))
